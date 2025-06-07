e06c3af7cbb77b6e8fd1bd128440bebe3d23373c
reylanceg <reylanceg@gmail.com>
2025-06-07 19:20:40 +0800

    Sorted sidebar.php for admin

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -6,49 +6,55 @@
 
 
     <div class="sidebar-left-options">
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><span class="material-symbols-sharp">
-                dashboard
-            </span>
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <span class="material-symbols-sharp">dashboard</span>
             <h3>Dashboard</h3>
         </a>
 
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><span class="material-symbols-sharp">
-                groups
-            </span>
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <span class="material-symbols-sharp">groups</span>
             <h3>Manage Users</h3>
         </a>
 
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><i
-                class="fa-solid fa-user-doctor"></i>Appointments</a>
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><i class="fa-solid fa-notes-medical"></i>Medical Records</a>
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><span class="material-symbols-sharp">
-                medication
-            </span>Prescriptions</a>
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><span class="material-symbols-sharp">
-                receipt_long
-            </span>Billing</a>
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><span class="material-symbols-sharp">
-                inventory
-            </span>Inventory</a>
-        <a href="#" class="side-link" onclick="setActiveLink(this)"><span class="material-symbols-sharp">
-                description
-            </span>Report</a>
-        <!-- <a href="#"><span class="material-symbols-sharp">
-                settings
-            </span>Settings</a>
-        <a href="#"><span class="material-symbols-sharp">
-                person
-            </span>My Account</a>
-        <a href="#"><span class="material-symbols-sharp">
-                logout
-            </span>Sign Out</a> -->
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <i class="fa-solid fa-user-doctor"></i>
+            <h3>Appointments</h3>
+        </a>
+        
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <i class="fa-solid fa-notes-medical"></i>
+            <h3>Medical Records</h3>
+        </a>
+        
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <span class="material-symbols-sharp">medication</span>
+            <h3>Prescriptions</h3>
+        </a>
+        
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <span class="material-symbols-sharp">receipt_long</span>
+            <h3>Billing</h3>
+        </a>
+
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <span class="material-symbols-sharp">inventory</span>
+            <h3>Inventory</h3>
+        </a>
+
+        <a href="#" class="side-link" onclick="setActiveLink(this)">
+            <span class="material-symbols-sharp">description</span>
+            <h3>Reports</h3>
+        </a>
+        
         <div class="sidebar-left-lower">
-            <a href="#" class="side-link" onclick="setActiveLink(this)"><span class="material-symbols-sharp">
-                help
-            </span>Help</a>
-        <a href="<?= URL_ROOT ?>/user/logout"><span class="material-symbols-sharp">
-logout
-</span>Logout</a>
+            <a href="#" class="side-link" onclick="setActiveLink(this)">
+                <span class="material-symbols-sharp">help</span>
+                <h3>Help</h3>
+            </a>
+            <a href="<?= URL_ROOT ?>/user/logout">
+                <span class="material-symbols-sharp">logout</span>
+                <h3>Logout</h3>
+            </a>
         </div>
     </div>
 
